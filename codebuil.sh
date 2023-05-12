a1d4fdd37c9c89f02c8b73fc0efaf748dd2062b9
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2023-05-12 00:38:32 -0400

    Create codebuil.sh

---
+++ codebuil.sh
@@ -0,0 +1,12 @@
+yum install java-1.8* -y git -y
+yum install maven -y
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.88/bin/apache-tomcat-8.5.88.tar.gz
+tar -xf apache-tomcat-8.5.88.tar.gz
+cd apache-tomcat-8.5.88.tar.gz/bin
+bash startup.sh
+git clone https://github.com/ramdevops03/Development-Team-Repo.git
+cd Development-Team-Repo
+mvn clean
+mvn package
+cd target
+cp web.war /root/apache-tomcat-8.5.88/webapps
